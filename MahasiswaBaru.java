a63311143c33cef59e7475e3f3f1c32e01f74f29
rikosgrt <122290423+rikosgrt@users.noreply.github.com>
2024-01-02 22:32:58 +0700

    Create MahasiswaBaru.java

---
+++ MahasiswaBaru.java
@@ -0,0 +1,25 @@
+
+package soal1;
+
+public class MahasiswaBaru extends Mahasiswa {
+    private int nim;
+
+    public MahasiswaBaru(String nama, int umur, int nim) {
+        super(nama, umur);
+        this.nim = nim;
+    }
+
+    public int getNim() {
+        return nim;
+    }
+
+    public void setNim(int nim) {
+        this.nim = nim;
+    }
+
+    @Override
+    public void displayInfo() {
+        super.displayInfo();
+        System.out.println("NIM: " + nim);
+    }
+}
